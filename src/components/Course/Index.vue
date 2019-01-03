1a5b2ea1735e9ece4714cc6196d183d568c55d0c
Artanic <599253487@qq.com>
2019-01-03 22:28:26 +0800

    update

--- src/components/Course/Index.vue
+++ src/components/Course/Index.vue
@@ -7,13 +7,13 @@
       </el-col>
       <el-col :span="2" style="float: right"><v-nav></v-nav></el-col>
     </el-row>
-    <el-row style="height: 100%">
+    <el-row style="height: 100%" :gutter="6">
       <el-col :span="4" style="margin-right: 10%">
           <el-menu class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse" style="float: left;margin: 0 5% 0 0">
             <v-aside :courseInformation="coInfo"></v-aside>
           </el-menu>
       </el-col>
-      <el-col :span="17">
+      <el-col :span="14">
          <v-main :courseState="coState" :courseInformation="coInfo"></v-main>
       </el-col>
     </el-row>
@@ -75,7 +75,7 @@ export default {
 </script>
 <style scoped>
   .el-menu-vertical-demo:not(.el-menu--collapse) {
-     width: 220px;
+    width: 220px;
     min-height: 1000px;
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -24,11 +24,12 @@ const vuexLocal = new VuexPersistence({
 const store = new Vuex.Store({
   state: {
     semester: '',
-    authorized: false,
+    authorized: 'false',
     root: false,
     uid: 'b3b17c00f16511e8b3dfdca9047a0f14',
     email: '',
     name: '',
+    courseId: 'CS110',
     student_id: '2018533110',
     instructor: 'true'
   },
