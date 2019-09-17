1c81a32015c93d5bee7c96b9f723c3b00a12bf67
Artanic <599253487@qq.com>
2019-09-17 16:14:40 +0800

    fix formatter

--- src/public/formateDate.js
+++ src/public/formateDate.js
@@ -1,26 +1,26 @@
-function formateDate (time, ISOformat) {
-  const pad = n => n<10 ? '0'+n : n
-  const sgn = n => n<0 ? '-' : '+'
+function formateDate (time, ISOformat = false) {
+  const pad = n => n < 10 ? '0' + n : n
+  const sgn = n => n < 0 ? '-' : '+'
   const useISO = ISOformat || false
 
-  const tz = time.getTimezoneOffset(),
-        tzH = parseInt(Math.abs(tz/60)),
-        tzM = Math.abs(tz%60)
+  let tz = time.getTimezoneOffset()
+  let tzH = parseInt(Math.abs(tz / 60), 10)
+  let tzM = Math.abs(tz % 60)
 
-  const YYYY = useISO ? time.getUTCFullYear() : time.getFullYear(),
-        MM = useISO ? time.getUTCMonth() : time.getMonth(),
-        DD = useISO ? time.getUTCDate() : time.getDate(),
-        hh = useISO ? time.getUTCHours() : time.getHours(),
-        mm = useISO ? time.getUTCMinutes() : time.getMinutes(),
-        ss = useISO ? time.getUTCSeconds() : time.getSeconds()
+  let YYYY = useISO ? time.getUTCFullYear() : time.getFullYear()
+  let MM = useISO ? time.getUTCMonth() : time.getMonth()
+  let DD = useISO ? time.getUTCDate() : time.getDate()
+  let hh = useISO ? time.getUTCHours() : time.getHours()
+  let mm = useISO ? time.getUTCMinutes() : time.getMinutes()
+  let ss = useISO ? time.getUTCSeconds() : time.getSeconds()
 
-  return YYYY+'-'
-  + pad(MM+1)+'-'
-  + pad(DD)+'T'
-  + pad(hh)+':'
-  + pad(mm)+':'
-  + pad(ss)+(
-    useISO?'Z':(sgn(-tz)+pad(tzH)+':'+pad(tzM))
+  return YYYY + '-' +
+    pad(MM + 1) + '-' +
+    pad(DD) + 'T' +
+    pad(hh) + ':' +
+    pad(mm) + ':' +
+    pad(ss) + (
+    useISO ? 'Z' : (sgn(-tz) + pad(tzH) + ':' + pad(tzM))
   )
 }
 
