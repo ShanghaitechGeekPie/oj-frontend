32cad0a42c3149d93b6c64c155c1369c63067062
Artanic <599253487@qq.com>
2019-12-01 23:31:58 +0800

    change html to vue fail

--- app/src/student/components/Submission/Main.vue
+++ app/src/student/components/Submission/Main.vue
@@ -5,7 +5,7 @@
         <el-card shadow="always">
           <el-row class="margins">
             <el-col>
-               <span class="title-main">{{ assignmentDetail.name }} Submission </span>
+              <span class="title-main">{{ assignmentDetail.name }} Submission </span>
             </el-col>
           </el-row>
         </el-card>
@@ -39,209 +39,217 @@
       </el-row>
       <el-row class="row-only">
         <el-col>
-            <el-table
+          <el-table
             :data="submission"
-            v-loading="loading"
+            :default-sort="{prop: 'date', order: 'descending'}"
             class="table-only"
-            :default-sort = "{prop: 'date', order: 'descending'}">
+            v-loading="loading">
             <el-table-column
-              prop="submission_time"
-              label="When"
               align="center"
+              label="When"
+              prop="submission_time"
               sortable>
             </el-table-column>
             <el-table-column
-              prop="commit_tag"
               align="center"
-              label="Git ID">
+              label="Git ID"
+              prop="commit_tag">
             </el-table-column>
             <el-table-column
-              prop="score"
               align="center"
               label="Score"
+              prop="score"
               sortable>
             </el-table-column>
             <el-table-column
-              prop="overall_score"
               align="center"
-              label="Full score">
+              label="Full score"
+              prop="overall_score">
             </el-table-column>
-              <el-table-column
-              prop="status"
+            <el-table-column
               align="center"
-              label="Status">
-              <template slot-scope="scope" >
+              label="Status"
+              prop="status">
+              <template slot-scope="scope">
                 <el-button :style="getStatus(scope.row.state, 'style')">
                   {{ getStatus(scope.row.state, 'title') }}
                 </el-button>
               </template>
-              </el-table-column>
+            </el-table-column>
             <el-table-column
-              prop="delta"
               align="center"
-              label="Delta">
+              label="Delta"
+              prop="delta">
               <template slot-scope="scope">
                 <ScoreDelta :delta="scope.row.delta"></ScoreDelta>
               </template>
             </el-table-column>
-              <el-table-column
-                fixed="right"
-                align="center"
-                label="Outputs">
-                <template slot-scope="scope">
-                  <el-button @click="showMessage(scope.row)" size="small">check</el-button>
-                </template>
-              </el-table-column>
+            <el-table-column
+              align="center"
+              fixed="right"
+              label="Outputs">
+              <template slot-scope="scope">
+                <el-button @click="showMessage(scope.row)" size="small">check</el-button>
+              </template>
+            </el-table-column>
           </el-table>
-          </el-col>
-        </el-row>
+        </el-col>
+      </el-row>
     </el-row>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
-import ScoreDelta from '../../../public/ScoreDelta'
+    import {mapState} from 'vuex'
+    import ScoreDelta from '../../../public/ScoreDelta'
 
-export default {
-  components: {
-    ScoreDelta
-  },
-  data () {
-    return {
-      submission: [
-        {
-          commit_tag: '',
-          course_uid: '',
-          message: '',
-          score: 0,
-          overall_score: 0,
-          submission_time: 0,
-          delta: 0
-        }
-      ],
-      assignmentDetail: {
-        uid: '',
-        course_uid: '',
-        name: '',
-        deadline: 0,
-        release_date: 0,
-        descr_link: ''
-      },
-      message: '',
-      email: '',
-      loading: true
-    }
-  },
-  computed: mapState({
-    getAss: state => state.assignments,
-    getCoInfo: state => state.coInfo,
-    getID: state => state.baseInfo.uid,
-    Api: state => state.api
-  }),
-  props: ['deliverDetail', 'deliverInfo'],
-  mounted () {
-    this.submission = this.deliverDetail
-    this.assignmentDetail = this.deliverInfo
-    this.axios({
-      method: 'GET',
-      url: `${this.Api}/student/${this.getID}`
-    }).then((response) => {
-      let index = response.data.email.indexOf('@')
-      this.email = response.data.email.slice(0, index)
-    }).catch((err) => {
-      this.$message({
-        type: 'error',
-        message: err,
-        showClose: true
-      })
-    })
-  },
-  methods: {
-    showMessage (data) {
-      this.message = data.message
-    },
-    lower (word) {
-      if (word === undefined) {
-        return ''
-      } else {
-        return word.toLowerCase()
-      }
-    },
-    renderMultipleLines (content) {
-      return content.trim().replace(/\n/g, '<br/>')
-    },
-    getStatus (statusCode, cate) {
-        if (cate === 'title') {
-            switch (statusCode) {
-              case 0:
-                  return 'NONE';
-              case 1:
-                  return 'PENDING';
-              case 2:
-                  return 'JUDGED';
-              case 3:
-                  return 'INVALID';
+    export default {
+        components: {
+            ScoreDelta
+        },
+        data() {
+            return {
+                submission: [
+                    {
+                        commit_tag: '',
+                        course_uid: '',
+                        message: '',
+                        score: 0,
+                        overall_score: 0,
+                        submission_time: 0,
+                        delta: 0
+                    }
+                ],
+                assignmentDetail: {
+                    uid: '',
+                    course_uid: '',
+                    name: '',
+                    deadline: 0,
+                    release_date: 0,
+                    descr_link: ''
+                },
+                message: '',
+                email: '',
+                loading: true
             }
-        } else {
-            let bgColor = '';
-            switch (statusCode) {
-              case 0:
-                  bgColor = 'black';
-                  break;
-              case 1:
-                  bgColor = '#E6A23C';
-                  break;
-              case 2:
-                  bgColor = '#67C23A';
-                  break;
-              case 3:
-                  bgColor = bgColor = '#F56C6C';
-                  break;
+        },
+        computed: mapState({
+            getAss: state => state.assignments,
+            getCoInfo: state => state.coInfo,
+            getID: state => state.baseInfo.uid,
+            Api: state => state.api
+        }),
+        props: ['deliverDetail', 'deliverInfo'],
+        mounted() {
+            this.submission = this.deliverDetail
+            this.assignmentDetail = this.deliverInfo
+            this.axios({
+                method: 'GET',
+                url: `${this.Api}/student/${this.getID}`
+            }).then((response) => {
+                let index = response.data.email.indexOf('@')
+                this.email = response.data.email.slice(0, index)
+            }).catch((err) => {
+                this.$message({
+                    type: 'error',
+                    message: err,
+                    showClose: true
+                })
+            })
+        },
+        methods: {
+            showMessage(data) {
+                this.message = data.message
+            },
+            lower(word) {
+                if (word === undefined) {
+                    return ''
+                } else {
+                    return word.toLowerCase()
+                }
+            },
+            renderMultipleLines(content) {
+                return content.trim().replace(/\n/g, '<br/>')
+            },
+            getStatus(statusCode, cate) {
+                if (cate === 'title') {
+                    switch (statusCode) {
+                        case 0:
+                            return 'NONE';
+                        case 1:
+                            return 'PENDING';
+                        case 2:
+                            return 'JUDGED';
+                        case 3:
+                            return 'INVALID';
+                    }
+                } else {
+                    let bgColor = '';
+                    switch (statusCode) {
+                        case 0:
+                            bgColor = 'black';
+                            break;
+                        case 1:
+                            bgColor = '#E6A23C';
+                            break;
+                        case 2:
+                            bgColor = '#67C23A';
+                            break;
+                        case 3:
+                            bgColor = bgColor = '#F56C6C';
+                            break;
+                    }
+                    return `background-color:${bgColor}; color:white; width:110px;opacity: 0.7;`
+                }
             }
-            return `background-color:${bgColor}; color:white; width:110px;opacity: 0.7;`
+        },
+        watch: {
+            deliverDetail: function (newValue) {
+                this.loading = false
+                this.submission = newValue
+                this.message = this.submission[0].message
+            }
         }
     }
-  },
-  watch: {
-    deliverDetail: function (newValue) {
-      this.loading = false
-      this.submission = newValue
-      this.message = this.submission[0].message
-    }
-  }
-}
 </script>
 
 <style scoped>
-.margins{
-  margin: 1% 0 1% 2%;
-}
+  .margins {
+    margin: 1% 0 1% 2%;
+  }
+
   .code {
     font-size: 15px;
     font-family: Consolas, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
     color: white;
     min-height: 400px;
   }
+
   .row-main {
     margin-bottom: 10px;
   }
+
   .title-main {
     font-size: 25px;
   }
+
   .card-only {
     background-color: black;
   }
+
   .table-only {
     width: 100%;
   }
+
   .row-only {
     margin-top: 2%;
   }
+
   .code-two {
     font-family: Consolas, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
     font-size: 15px;
   }
+
   .code-box {
     min-height: 150px;
   }
